730e56b6447e23c506bcf70cf8be46f6b5c60121
Cara-Notte <caranottella@gmail.com>
2026-06-17 01:43:48 +0700

    fix: import JSON decoder extension

--- src/main/kotlin/darcy/veterinary/infrastructure/storage/JsonClinicStorage.kt
+++ src/main/kotlin/darcy/veterinary/infrastructure/storage/JsonClinicStorage.kt
@@ -18,6 +18,7 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.time.LocalDateTime
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
