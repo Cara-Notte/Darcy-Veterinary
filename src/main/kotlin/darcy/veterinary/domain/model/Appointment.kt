1924de6542fe93c51c2972d0136c430513ff047b
Cara-Notte <caranottella@gmail.com>
2026-06-16 23:50:12 +0700

    feat: add appointment model

---
+++ src/main/kotlin/darcy/veterinary/domain/model/Appointment.kt
@@ -0,0 +1,11 @@
+package darcy.veterinary.domain.model
+
+import java.time.LocalDateTime
+
+data class Appointment(
+    val id: String,
+    val petId: String,
+    val scheduledAt: LocalDateTime,
+    val reason: String,
+    val status: AppointmentStatus = AppointmentStatus.SCHEDULED
+)
