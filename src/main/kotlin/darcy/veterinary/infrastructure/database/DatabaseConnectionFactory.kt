e1ce01ed50e4147c46deda9ce397cb2fb4fee8e2
Cara-Notte <caranottella@gmail.com>
2026-06-20 07:24:59 +0700

    feat: open SQLite connections

--- src/main/kotlin/darcy/veterinary/infrastructure/database/DatabaseConnectionFactory.kt
+++ src/main/kotlin/darcy/veterinary/infrastructure/database/DatabaseConnectionFactory.kt
@@ -1,5 +1,16 @@
 package darcy.veterinary.infrastructure.database
 
+import java.nio.file.Files
+import java.sql.Connection
+import java.sql.DriverManager
+
 class DatabaseConnectionFactory(
     private val config: DatabaseConfig
-)
+) {
+    fun openConnection(): Connection {
+        config.databasePath.parent?.let { parent ->
+            Files.createDirectories(parent)
+        }
+        return DriverManager.getConnection("jdbc:sqlite:" + config.databasePath.toString())
+    }
+}
