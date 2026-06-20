30966651cec929296fbf4f02e3e273f9788f7a46
Cara-Notte <caranottella@gmail.com>
2026-06-20 07:25:45 +0700

    feat: add default database paths

--- src/main/kotlin/darcy/veterinary/infrastructure/database/DatabaseConfig.kt
+++ src/main/kotlin/darcy/veterinary/infrastructure/database/DatabaseConfig.kt
@@ -3,6 +3,6 @@ package darcy.veterinary.infrastructure.database
 import java.nio.file.Path
 
 data class DatabaseConfig(
-    val databasePath: Path,
-    val backupDirectory: Path
+    val databasePath: Path = Path.of("data", "darcy-vet.db"),
+    val backupDirectory: Path = Path.of("data", "backups")
 )
